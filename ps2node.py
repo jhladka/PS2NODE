9cffc78ef9dc30a6c25307c522cf1915507b6c45
jhladka <jarka.repakova@centrum.cz>
2017-02-25 09:32:16 +0100

    Add more usage info

--- ps2node.py
+++ ps2node.py
@@ -41,6 +41,20 @@ def get_input():
 ./ps2node.py --lscpu lscpu.txt < ps.log
 or
 ps -L -o pid, lwp, psr, comm -p ${PID} | ./ps2node.py --lscpu <(lscpu)
+
+To process many files and replace original files:
+
+for file in $(find ./ -name "*ps.log") ; do
+  temp_file=$(mktemp)
+  ./ps2node.py --lscpu lscpu.txt <"${file}" >"${temp}"
+  if [[ $? == 0 ]] && [[ -s "${temp_file}" ]] ; then
+    mv "${temp_file}" "${file}"
+  else
+    echo "ps2node.py has failed for file \"${file}\"" 1>&2
+    rm "${temp_file}"
+  fi
+done
+
 """
 
     description = """Converts output of ps -L -o pid, lwp, psr, comm -p
